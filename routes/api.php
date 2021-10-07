067a27b6f092a4a0eaea57452b7047f0680ae2b8
julialunati <julia.molokova11@icloud.com>
2021-10-07 19:57:54 +0200

    created ItemController, Item Model, basic routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/items', [ItemController::class, 'index']);
+Route::prefix('/item')->group(function () {
+        Route::post('/store', [ItemController::class, 'store']);
+        Route::put('/{id}', [ItemController::class, 'update']);
+        Route::delete('/{id}', [ItemController::class, 'destroy']);
+    }
+);
+
+// Route::group(['prefix'=>'item'], function(){
+//     Route::post('/store', [ItemController::class, 'store']);
+//     Route::put('/{id}', [ItemController::class, 'update']);
+//     Route::delete('/{id}', [ItemController::class, 'destroy']);
+// });
