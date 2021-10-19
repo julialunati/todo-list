2ab325cac1ba2c1054a30aa5d46821b630fb0ba0
julialunati <julia.molokova11@icloud.com>
2021-10-19 16:45:20 +0200

    connect & basic Vue layout

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ // for the version Vue3
+ 
+ mix.js('resources/js/app.js', 'public/js').vue()
+ .postCss('resources/css/app.css', 'public/css', [
+     //
+ ]);
